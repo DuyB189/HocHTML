b61ce5edfc1ab95c782d7214414042e76032e3a2
DuyB189 <bduy.le189@gmail.com>
2022-11-11 15:42:04 +0700

    11/11/2022 - 15:42

---
+++ index.js
@@ -0,0 +1,10 @@
+//let age = window.prompt("Phần mềm tính tuổi, xin hãy nhập tuổi của bạn: "); //string
+//age = Number(age); // type conversion string to number
+//age +=1;    // because age is number so we can plus 
+//alert(`Xin chúc mừng bạn đã ${age} tuổi rồi!!!`); // age + 1
+
+function test() {
+    console.log("1");
+    console.log("2");
+    console.log("3");
+}
